1142a4603e45aac00ca66db4b0532837a6730cc7
Debasish Das <110322488+DebasishDas97@users.noreply.github.com>
2022-10-19 11:04:47 +0530

    Update Quiz.js

--- Components/Quiz.js
+++ Components/Quiz.js
@@ -1,27 +1,25 @@
-import React, { useState } from "react"
+import { useState } from "react"
 
 export default function Quiz(props) {
     const [quizEnded, setQuizEnded] = useState(false)
     const [countCorrectAnswer, setCountCorrectAnswer] = useState(0)
-    
+
     function handleClick(id, quizId) {
-        const option = props.quizElements.map(item => item.correctAnswerId)
-        if (option.includes(id)) {
-            setCountCorrectAnswer(prevCountCorrectAnswer => prevCountCorrectAnswer + 1)
-        }
-        props.quizSetter(prevData => prevData.map(item => item.id===quizId ? {...item, selectedAnswerId: id} : item))
+        props.quizSetter(prevData => prevData.map(item => item.id === quizId ? { ...item, selectedAnswerId: id } : item))
     }
-    
+
     function getMyClass(selectedId, correctId, answerId) {
-       if(answerId === selectedId) {
-           return {backgroundColor: "#4D5B9E"}
-       }
-       if(correctId === answerId && quizEnded){
-           return {backgroundColor: "green"}
-       }
+        if (answerId === selectedId) {
+            return { backgroundColor: "#4D5B9E" }
+        }
+        if (correctId === answerId && quizEnded) {
+            return { backgroundColor: "green" }
+        }
     }
-    
+
     function checkAnswers() {
+        setCountCorrectAnswer(() => props.quizElements.reduce((prevCorrectCountAnswer, item) => (item.selectedAnswerId === item.correctAnswerId ? prevCorrectCountAnswer + 1 : prevCorrectCountAnswer),0)) 
+
         setQuizEnded(true)
         if (quizEnded) {
             props.onClicked()
@@ -34,27 +32,27 @@ export default function Quiz(props) {
         <div>
             <div className="quiz-section">
                 {props.quizElements.map(quiz => (
-                   <div key={quiz.id}>
-  <h3 className="question" key={quiz.question}>
-    {quiz.question}
-  </h3>
-  {quiz.answers.map(answer => {
-    return (
-      <button disabled={quizEnded} style={getMyClass(quiz.selectedAnswerId, quiz.correctAnswerId, answer.id)}
-        className="options"
-        onClick={() => handleClick(answer.id, quiz.id)} key={answer.id}
-        id={answer.id}
-       >{answer.value}
-        <br/> 
-        </button>
-      )
-    })}
-  <hr />
-</div>
+                    <div key={quiz.id}>
+                        <h3 className="question" key={quiz.question}>
+                            {quiz.question}
+                        </h3>
+                        {quiz.answers.map(answer => {
+                            return (
+                                <button disabled={quizEnded} style={getMyClass(quiz.selectedAnswerId, quiz.correctAnswerId, answer.id)}
+                                    className="options"
+                                    onClick={() => handleClick(answer.id, quiz.id)} key={answer.id}
+                                    id={answer.id}
+                                >{answer.value}
+                                    <br />
+                                </button>
+                            )
+                        })}
+                        <hr />
+                    </div>
                 ))}
-<div className="check-answer-btn">
+                <div className="check-answer-btn">
                     <h2> {quizEnded ? `Your score is ${countCorrectAnswer}/5.` : "Select Options To Check Answers"}</h2>
-                    <button className="blue-btn" onClick={checkAnswers} >{!quizEnded? "Check Answers" : "Play Again"} </button>
+                    <button className="blue-btn" onClick={checkAnswers} >{!quizEnded ? "Check Answers" : "Play Again"} </button>
                 </div>
             </div>
         </div>
